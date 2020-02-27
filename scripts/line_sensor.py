1b9c8b10b112bfddbbe2b9a386f54863248363fb
Nathaniel Zollinger <nathaniel.zollinger@huskers.unl.edu>
2020-02-27 16:20:52 -0600

    added mapPacket publishing

--- scripts/line_sensor.py
+++ scripts/line_sensor.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from lab2.msg import balboaLL # import balboa message
+from lab2.msg import mapPacket # import mapPacket message
 
 def parse_balboa_msg(data, self):
+    self.threshold = rospy.get_param('threshold') # get line threshold
+
+    # update sensor array with message data
     self.s[0] = data.sensor1
     self.s[1] = data.sensor2
     self.s[2] = data.sensor3
     self.s[3] = data.sensor4
     self.s[4] = data.sensor5
 
+    # check if greater than threshold and update map array
     for i in range (0, 5):
         if self.s[i] > self.threshold:
-            self.line[i] = 1
+            self.map[i] = 1
         else:
-            self.line[i] = 0
+            self.map[i] = 0
+
+    # update mapPacket message with map values
+    self.col.row1 = self.map[0]
+    self.col.row2 = self.map[1]
+    self.col.row3 = self.map[2]
+    self.col.row4 = self.map[3]
+    self.col.row5 = self.map[4]
+
+    # publish mapPacket
+    self.mapPub.publish(self.col)
 
 class TheNode(object):
   # This class holds the rospy logic for sending line sensor results 
@@ -23,12 +38,14 @@ class TheNode(object):
 
     rospy.init_node('line_sensor') # intialize node
     
-    # initialize publisher node for distance PID controller
-    self.line_reading = rospy.Publisher('/dist', pid_input, queue_size=10)
+    # initialize publisher node for map packets
+    self.mapPub = rospy.Publisher('/mapping', mapPacket, queue_size=10)
 
-    self.threshold = rospy.get_param('threshold') # init distance target
-    self.line = [0, 0, 0, 0, 0]
-    self.s = [0, 0, 0, 0, 0]
+    self.threshold = rospy.get_param('threshold') # init line threshold
+    self.map = [0, 0, 0, 0, 0] # init map array
+    self.s = [0, 0, 0, 0, 0] # init sensor array
+    self.col = mapPacket() # init default mapPacket message
+
   def main_loop(self):
     # initialize subscriber node for messages from balboa robot
     rospy.Subscriber('balboaLL', balboaLL, parse_balboa_msg, self)
