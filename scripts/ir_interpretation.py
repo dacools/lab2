f55dacd0eaa9b47fd037925277ff507203946116
Nathaniel Zollinger <nathaniel.zollinger@huskers.unl.edu>
2020-03-06 15:01:55 -0600

    finished tuning reactive control

--- scripts/ir_interpretation.py
+++ scripts/ir_interpretation.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
 import rospy
 from lab2.msg import balboaLL # import balboa message
-from std_msgs.msg import Float32 # import Float32
-from math import pow
+from std_msgs.msg import Float32 # import Float32 message
+from math import pow # import power function
 
 def parse_balboa_msg(data, self):
-    self.IR = data.IR
+    self.IR = data.IR # analog reading (0-1023)
+    self.IR = self.IR * (5.0 / 1023.0) # convert to voltage
+
     # Voltage is converted to distance (cm) (Less than 15 cm is not accurate)
     self.distance = self.P1*pow(self.IR,4) + self.P2*pow(self.IR,3) +self.P3*pow(self.IR,2)+self.P4*self.IR+self.P5
 
-    self.irPub.publish(self.distance)
+    self.irPub.publish(self.distance) # publish ir distance reading
     
 class TheNode(object):
-  # This class holds the rospy logic for receiving voltages from an IR sensor,
-  # then converting the voltage to a distance and then publishing the distance 
+    # This class holds the rospy logic for receiving analog readings from an IR sensor,
+    # converting the reading to a distance and publishing the distance 
 
-  def __init__(self):
+    def __init__(self):
 
-    rospy.init_node('ir_interpretation') # intialize node
+        rospy.init_node('ir_interpretation') # intialize node
 
-    # initialize publisher node for IR distances 
-    self.irPub = rospy.Publisher('/ir_distance', Float32, queue_size=10)
-    self.IR = 0
-    self.distance = Float32()
+        # initialize publisher node for IR distances 
+        self.irPub = rospy.Publisher('/ir_distance', Float32, queue_size=10)
+        self.IR = 0 # init ir reading variable
+        self.distance = Float32() # init distance message
 
-    # Curve fit parameters for a 4th order curve fit
-    self.P1 = 20.4560 
-    self.P2 = -159.9982  
-    self.P3 = 459.6192 
-    self.P4 = -597.1905  
-    self.P5 = 340.1046
+        # Curve fit parameters for a 4th order curve fit
+        self.P1 = 20.4560 
+        self.P2 = -159.9982  
+        self.P3 = 459.6192 
+        self.P4 = -597.1905  
+        self.P5 = 340.1046
 
-  def main_loop(self):
-    # initialize subscriber node for messages from balboa robot
-    rospy.Subscriber('balboaLL', balboaLL, parse_balboa_msg, self)
+    def main_loop(self):
+        # initialize subscriber node for messages from balboa robot
+        rospy.Subscriber('balboaLL', balboaLL, parse_balboa_msg, self)
 
-    rospy.spin() # wait for messages
+        rospy.spin() # wait for messages
 
 if __name__ == '__main__':
     try:
--- scripts/reactive_control.py
+++ scripts/reactive_control.py
@@ -1,35 +1,59 @@
 #!/usr/bin/env python
 import rospy
+from lab2.msg import balboaLL # import balboa message
 from std_msgs.msg import Float32 # import Float32
 
-def parse_ir_distance_msg(data, self):
-    self.dist_target = rospy.get_param("distance/target")
-    self.distance = data
-    diff = self.target - float(self.distance) # compute how far off the following target is
+def parse_balboa_msg(data, self):
+    curr_ms = data.arduinoMillis
+    if curr_ms - self.last_ms > 100:
+        # one second since last reaction
+        self.react = True
+        self.last_ms = curr_ms
 
-    # Determine if the difference is enough to make the robot want to move
-    if diff > 5:
-        self.dist_target = self.dist_target + diff
-        rospy.set_param("distance/target",self.dist_target) # publish the new target
-    elif diff < 5:
-        self.dist_target = self.dist_target - diff
-        rospy.set_param("distance/target",self.dist_target) # publish the new target
+def parse_ir_distance_msg(data, self):
+    if self.react:
+        self.react = False # reset reaction
+        self.dist_target = rospy.get_param('distance/target') # get current distance target
+        self.ir_target = rospy.get_param('reactive/target') # get current reactive target
+        self.ir_dist = data.data # get current ir distance
+        diff = self.ir_target - self.ir_dist # compute ir target error
+        off = False # debugging
+
+        if self.ir_dist < 15:
+            # out of calibration range, do nothing
+            off = True
+        elif self.ir_dist > 60:
+            # out of useful range, do nothing
+            off = True
+        elif diff > 2:
+            self.dist_target = self.dist_target + 5 # add offset to distance target
+        elif diff < -2:
+            self.dist_target = self.dist_target - 5 # subtract offset to distance target
+
+        rospy.set_param('distance/target',self.dist_target) # publish new distance target
+
+        rospy.set_param('debug/diff',diff)
+        rospy.set_param('debug/off',off)
 
 class TheNode(object):
-  # This class holds the rospy logic for following behind something a set distance 
+    # This class holds the rospy logic for following at a set distance 
 
-  def __init__(self):
+    def __init__(self):
 
-    rospy.init_node('reactive_control') # intialize node
-    self.dist_target = rospy.get_param("distance/target")
-    self.distance = 0
-    self.target = rospy.get_param('reactive_control_target') # init reactive control target
+        rospy.init_node('reactive_control') # intialize node
 
-  def main_loop(self):
-    # initialize subscriber node for messages from the ir_distance converter
-    rospy.Subscriber('/ir_distance', Float32, parse_ir_distance_msg, self)
+        self.dist_target = rospy.get_param('distance/target') # init distance target
+        self.ir_target = rospy.get_param('reactive/target') # init reactive target
+        self.ir_dist = 0 # init ir distance variable
+        self.react = False # init variable to delay reaction
+        self.last_ms = 0 # init millis variable
 
-    rospy.spin() # wait for messages
+    def main_loop(self):
+        # initialize subscriber node for messages from the ir_distance converter
+        rospy.Subscriber('balboaLL', balboaLL, parse_balboa_msg, self)
+        rospy.Subscriber('/ir_distance', Float32, parse_ir_distance_msg, self)
+
+        rospy.spin() # wait for messages
 
 if __name__ == '__main__':
     try:
